4c70140dbfbc880aac5cd4ddb178161c27264c60
Rajendran R <rajendranr-5483@users.noreply.github.com>
2020-10-26 11:59:07 +0530

    Files added to perform CRUD using URL adaptor of data manager.

---
+++ Data/OrderDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_Kanban_Crud_UrlAdaptor.Data
+{
+    public class Order
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+        public string ShipName { get; set; }
+        public string ShipCity { get; set; }
+
+    }
+}
